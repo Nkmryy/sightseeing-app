e0a43d98cf41cce600ec67793db15deb950ceb45
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-11 18:41:05 +0900

    Active Storageの導入

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,4 +9,9 @@ class TripsController < ApplicationController
   def create
   end
 
+  private
+
+  def trip_params
+    params.require(:trip).parmit(:attraction_id, :evaluation_id, :impressions, :tytle, :image).merge(user_id: current_user.id)
+  end
 end
