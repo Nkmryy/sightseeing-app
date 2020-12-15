89fa3eb47348cbb07923b7bb9dbb148f7db98132
Nkmryy <yayoi.nakamura.8669@gmail.com>
2020-12-15 16:06:23 +0900

    コントローラーの定義２

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
+    redirect_to trip_path(comment.trip_id)
   end
 
   private
